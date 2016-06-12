9fb2b576278c040053d4266c2b8480f2ef6b9492
manami.project <manami.project@posteo.net>
2016-06-12 16:29:15 +0200

    Changed: Use apache commons-validator for url validation

--- cache/src/main/java/io/github/manami/cache/extractor/HeadlessBrowser.java
+++ cache/src/main/java/io/github/manami/cache/extractor/HeadlessBrowser.java
@@ -2,6 +2,8 @@ package io.github.manami.cache.extractor;
 
 import javax.inject.Named;
 
+import org.apache.commons.validator.routines.UrlValidator;
+
 import com.ui4j.api.browser.BrowserEngine;
 import com.ui4j.api.browser.BrowserFactory;
 import com.ui4j.api.browser.Page;
@@ -13,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
  * A Browser which lets you download the various anime sites. It is necessary to
  * use this approach, because DDoS prevention services require the site be
  * rendered.
- *
+ * 
  * @author manami-project
  * @since 2.0.0
  */
@@ -21,24 +23,27 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HeadlessBrowser {
 
+    private static final String[] VALID_SCHEMES = new String[] { "HTTP", "HTTPS" };
+    private final UrlValidator urlValidator;
     private BrowserEngine browserEngine;
 
 
     public HeadlessBrowser() {
+        urlValidator = new UrlValidator(VALID_SCHEMES);
         Platform.runLater(() -> browserEngine = BrowserFactory.getWebKit());
     }
 
 
     /**
      * Downloads the site and returns it.
-     *
+     * 
      * @since 2.0.0
      * @param url
      *            URL
      * @return Plain xml text of the website.
      */
     public String pageAsString(final String url) {
-        if (!url.startsWith("http")) {
+        if (!urlValidator.isValid(url)) {
             log.warn("Seems not be a valid URL: {}", url);
             return null;
         }
