dc39659954598088e7031bfc4ef33866e5a8606e
manami.project <manami.project@posteo.net>
2017-05-14 20:27:27 +0200

    Added: assertj to core

--- core/src/test/java/io/github/manami/core/config/ConfigFileWatchdogTest.java
+++ core/src/test/java/io/github/manami/core/config/ConfigFileWatchdogTest.java
@@ -1,51 +1,50 @@
 package io.github.manami.core.config;
 
 import static io.github.manami.dto.TestConst.UNIT_TEST_GROUP;
-import static org.testng.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import org.testng.annotations.Test;
 
 public class ConfigFileWatchdogTest {
 
-    @Test(groups = UNIT_TEST_GROUP, expectedExceptions = { IllegalStateException.class })
-    public void ConfigFileWatchdog() {
-        // given
+  @Test(groups = UNIT_TEST_GROUP, expectedExceptions = {IllegalStateException.class})
+  public void ConfigFileWatchdog() {
+    // given
 
-        // when
-        new ConfigFileWatchdog(null);
+    // when
+    new ConfigFileWatchdog(null);
 
-        // then
-    }
+    // then
+  }
 
 
-    @Test(groups = UNIT_TEST_GROUP)
-    public void validate() throws IOException {
-        // given
-        final Path createTempDirectory = Files.createTempDirectory("test");
+  @Test(groups = UNIT_TEST_GROUP)
+  public void validate() throws IOException {
+    // given
+    final Path createTempDirectory = Files.createTempDirectory("test");
 
-        // when
-        new ConfigFileWatchdog(createTempDirectory).validate();
+    // when
+    new ConfigFileWatchdog(createTempDirectory).validate();
 
-        // then
-        final Path configFolder = createTempDirectory.resolve("config");
-        final Path themeFolder = configFolder.resolve("theme");
-        final Path dtdFile = configFolder.resolve("animelist.dtd");
-        final Path transformationFile = themeFolder.resolve("animelist_transform.xsl");
-        final Path stylesheetFile = themeFolder.resolve("style.css");
+    // then
+    final Path configFolder = createTempDirectory.resolve("config");
+    final Path themeFolder = configFolder.resolve("theme");
+    final Path dtdFile = configFolder.resolve("animelist.dtd");
+    final Path transformationFile = themeFolder.resolve("animelist_transform.xsl");
+    final Path stylesheetFile = themeFolder.resolve("style.css");
 
-        assertTrue(Files.exists(configFolder));
-        assertTrue(Files.isDirectory(configFolder));
-        assertTrue(Files.exists(themeFolder));
-        assertTrue(Files.isDirectory(themeFolder));
-        assertTrue(Files.exists(dtdFile));
-        assertTrue(Files.isRegularFile(dtdFile));
-        assertTrue(Files.exists(transformationFile));
-        assertTrue(Files.isRegularFile(transformationFile));
-        assertTrue(Files.exists(stylesheetFile));
-        assertTrue(Files.isRegularFile(stylesheetFile));
-    }
+    assertThat(Files.exists(configFolder)).isTrue();
+    assertThat(Files.isDirectory(configFolder)).isTrue();
+    assertThat(Files.exists(themeFolder)).isTrue();
+    assertThat(Files.isDirectory(themeFolder)).isTrue();
+    assertThat(Files.exists(dtdFile)).isTrue();
+    assertThat(Files.isRegularFile(dtdFile)).isTrue();
+    assertThat(Files.exists(transformationFile)).isTrue();
+    assertThat(Files.isRegularFile(transformationFile)).isTrue();
+    assertThat(Files.exists(stylesheetFile)).isTrue();
+    assertThat(Files.isRegularFile(stylesheetFile)).isTrue();
+  }
 }
