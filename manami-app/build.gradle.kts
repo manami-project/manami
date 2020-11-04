6bb99b7d8088c308d6712e3f804d3abc0f4879b5
manami-project <13179375+manami-project@users.noreply.github.com>
2020-11-04 19:45:50 +0100

    Updated dependencies

--- manami-app/build.gradle.kts
+++ manami-app/build.gradle.kts
@@ -11,15 +11,15 @@ repositories {
 
 dependencies {
     api(kotlin("stdlib-jdk8"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("io.github.manamiproject:modb-core:2.3.2")
-    api("io.github.manamiproject:modb-anidb:1.0.11")
-    api("io.github.manamiproject:modb-anilist:2.0.0")
-    api("io.github.manamiproject:modb-anime-planet:1.0.7")
-    api("io.github.manamiproject:modb-kitsu:1.0.11")
-    api("io.github.manamiproject:modb-mal:1.0.11")
-    api("io.github.manamiproject:modb-notify:1.0.11")
-    api("io.github.manamiproject:modb-db-parser:1.0.6")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0")
+    implementation("io.github.manamiproject:modb-core:2.3.3")
+    api("io.github.manamiproject:modb-anidb:1.0.13")
+    api("io.github.manamiproject:modb-anilist:2.0.1")
+    api("io.github.manamiproject:modb-anime-planet:1.0.11")
+    api("io.github.manamiproject:modb-kitsu:1.0.13")
+    api("io.github.manamiproject:modb-mal:1.0.12")
+    api("io.github.manamiproject:modb-notify:1.0.12")
+    api("io.github.manamiproject:modb-db-parser:1.0.7")
 
     testImplementation("io.github.manamiproject:modb-test:1.1.0")
 }
