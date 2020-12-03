c4ee894132e910dbd33e8c433d88a79bc65aea24
manami-project <13179375+manami-project@users.noreply.github.com>
2020-12-03 20:07:56 +0100

    Use typealias for title in AnimeListEntry

--- manami-app/src/main/kotlin/io/github/manamiproject/manami/app/models/AnimeListEntry.kt
+++ manami-app/src/main/kotlin/io/github/manamiproject/manami/app/models/AnimeListEntry.kt
@@ -2,11 +2,12 @@ package io.github.manamiproject.manami.app.models
 
 import io.github.manamiproject.modb.core.models.Anime
 import io.github.manamiproject.modb.core.models.Episodes
+import io.github.manamiproject.modb.core.models.Title
 import java.net.URI
 
 data class AnimeListEntry(
         private val link: LinkEntry = NoLink,
-        private val title: String,
+        private val title: Title,
         private val episodes: Episodes,
         private val type: Anime.Type,
         private val location: String,
