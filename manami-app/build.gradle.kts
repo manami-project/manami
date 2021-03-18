21626f2a9e789e400422803197b08d454e67f927
manami-project <13179375+manami-project@users.noreply.github.com>
2021-03-18 20:51:56 +0100

    Updated dependencies

--- manami-app/build.gradle.kts
+++ manami-app/build.gradle.kts
@@ -11,21 +11,21 @@ repositories {
 
 dependencies {
     api(kotlin("stdlib-jdk8"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
-    api("io.github.manamiproject:modb-core:3.1.1")
-    api("io.github.manamiproject:modb-db-parser:2.0.3")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    api("io.github.manamiproject:modb-core:3.2.1")
+    api("io.github.manamiproject:modb-db-parser:2.0.4")
     api("org.slf4j:slf4j-api:1.7.30")
 
     implementation(kotlin("reflect"))
-    implementation("io.github.manamiproject:modb-anidb:2.0.3")
-    implementation("io.github.manamiproject:modb-anilist:3.0.3")
-    implementation("io.github.manamiproject:modb-anime-planet:2.0.3")
-    implementation("io.github.manamiproject:modb-kitsu:2.0.3")
-    implementation("io.github.manamiproject:modb-mal:2.0.4")
-    implementation("io.github.manamiproject:modb-notify:2.0.3")
+    implementation("io.github.manamiproject:modb-anidb:2.1.0")
+    implementation("io.github.manamiproject:modb-anilist:3.0.4")
+    implementation("io.github.manamiproject:modb-anime-planet:2.0.4")
+    implementation("io.github.manamiproject:modb-kitsu:2.0.4")
+    implementation("io.github.manamiproject:modb-mal:2.0.5")
+    implementation("io.github.manamiproject:modb-notify:2.0.4")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    testImplementation("io.github.manamiproject:modb-test:1.2.1")
+    testImplementation("io.github.manamiproject:modb-test:1.2.2")
 }
 
 object Versions {
