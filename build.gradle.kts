1672f37a2463aa6d6bee45275fd5a94bbfbfebc4
manami-project <13179375+manami-project@users.noreply.github.com>
2021-11-21 18:01:23 +0100

    Fixed fatjar build

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,100 @@ allprojects {
     version = project.findProperty("release.version") as String? ?: ""
 }
 
+val githubUsername = "manami-project"
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "modb-anidb"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-anidb")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-anilist"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-anilist")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-anime-planet"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-anime-planet")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-anisearch"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-anisearch")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-core"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-core")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-db-parser"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-db-parser")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-kitsu"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-kitsu")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-livechart"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-livechart")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-mal"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-mal")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-notify"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-notify")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+    maven {
+        name = "modb-test"
+        url = uri("https://maven.pkg.github.com/$githubUsername/modb-test")
+        credentials {
+            username = parameter("GH_USERNAME", githubUsername)
+            password = parameter("GH_PACKAGES_READ_TOKEN")
+        }
+    }
+}
+
 dependencies {
     api(project(":manami-gui"))
     api(project(":manami-app"))
@@ -31,4 +125,18 @@ tasks {
         }
         exclude(".gitemptydir")
     }
+}
+
+fun parameter(name: String, default: String = ""): String {
+    val env = System.getenv(name) ?: ""
+    if (env.isNotBlank()) {
+        return env
+    }
+
+    val property = project.findProperty(name) as String? ?: ""
+    if (property.isNotEmpty()) {
+        return property
+    }
+
+    return default
 }
