01c57082bf3c0ab7f36918270b66426985a41350
manami-project <13179375+manami-project@users.noreply.github.com>
2025-01-09 21:47:33 +0100

    Use public json api to check the most recent version

--- manami-app/src/main/kotlin/io/github/manamiproject/manami/app/versioning/GithubVersionProvider.kt
+++ manami-app/src/main/kotlin/io/github/manamiproject/manami/app/versioning/GithubVersionProvider.kt
@@ -1,22 +1,28 @@
 package io.github.manamiproject.manami.app.versioning
 
-import io.github.manamiproject.modb.core.extensions.EMPTY
 import io.github.manamiproject.modb.core.httpclient.DefaultHttpClient
 import io.github.manamiproject.modb.core.httpclient.HttpClient
+import io.github.manamiproject.modb.core.json.Json
+import io.github.manamiproject.modb.core.logging.LoggerDelegate
 import kotlinx.coroutines.runBlocking
 import java.net.URI
 
 internal class GithubVersionProvider(
-    private val uri: URI = URI("https://github.com/manami-project/manami/releases/latest"),
+    private val uri: URI = URI("https://api.github.com/repos/manami-project/manami/releases/latest"),
     private val httpClient: HttpClient = DefaultHttpClient.instance,
 ): VersionProvider {
 
     override fun version(): SemanticVersion {
-        val response = runBlocking { httpClient.get(uri.toURL()) }
-        check(response.isOk()) { "Unable to check latest version, because response code wasn't 200." }
+        return runBlocking {
+            val response = httpClient.get(uri.toURL())
+            check(response.isOk()) { "Unable to check latest version, because response code wasn't 200." }
 
-        val title = Regex("<title>.*?</title>").find(response.bodyAsText)?.value ?: EMPTY
-        val rawVersion = Regex("([0-9]+\\.?){3}").find(title)?.value ?: EMPTY
-        return  SemanticVersion(rawVersion)
+            val version =  Json.parseJson<GithubResponse>(response.bodyAsText)!!.name
+            SemanticVersion(version)
+        }
     }
 }
+
+private data class GithubResponse(
+    val name: String,
+)
