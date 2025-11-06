87afc26a6645b9f1d4e13a348e645414a25e9429
manami-project <13179375+manami-project@users.noreply.github.com>
2025-11-06 21:48:17 +0100

    4.0.0: Moved UnsavedChangesDialog into the correct package

--- manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/components/unsavedchangesdialog/UnsavedChangesDialog.kt
+++ manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/components/unsavedchangesdialog/UnsavedChangesDialog.kt
@@ -1,4 +1,4 @@
-package io.github.manamiproject.manami.gui.components
+package io.github.manamiproject.manami.gui.components.unsavedchangesdialog
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
