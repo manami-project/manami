28b3e042f9b8a804e9c1e3fe21664e190d56f229
manami-project <13179375+manami-project@users.noreply.github.com>
2025-11-07 11:46:39 +0100

    4.0.0: Fixed scrolling upwards always snapping back down

--- manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/components/simpletable/SimpleTable.kt
+++ manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/components/simpletable/SimpleTable.kt
@@ -1,8 +1,15 @@
 package io.github.manamiproject.manami.gui.components.simpletable
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.manamiproject.manami.gui.theme.ManamiTheme
@@ -12,21 +19,29 @@ internal fun SimpleTable(
     data: Map<String, Any>,
     config: SimpleTableConfig.() -> Unit = {},
 ) {
+    val listState = rememberLazyListState()
     val dataList = data.toList()
 
     ManamiTheme {
-        LazyColumn(Modifier.padding(16.dp)) {
-            item {
-                SimpleTableHeaderRow(
-                    config = config,
-                )
+        Box(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(Modifier.padding(16.dp)) {
+                item {
+                    SimpleTableHeaderRow(
+                        config = config,
+                    )
+                }
+                items(items = dataList, key = { it.first }) { entry ->
+                    SimpleTableRow(
+                        config = config,
+                        data = entry
+                    )
+                }
             }
-            items(dataList.size) { index ->
-                SimpleTableRow(
-                    config = config,
-                    data = dataList[index]
-                )
-            }
+
+            VerticalScrollbar(
+                adapter = rememberScrollbarAdapter(listState),
+                modifier = Modifier.align(Alignment.CenterEnd),
+            )
         }
     }
 }
