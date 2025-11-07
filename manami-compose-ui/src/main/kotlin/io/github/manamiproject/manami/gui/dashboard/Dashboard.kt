24e61b5b820b22db8fbf92fd989ec59ecbf78878
manami-project <13179375+manami-project@users.noreply.github.com>
2025-11-07 17:50:49 +0100

    4.0.0: Replaced 'Loading' text with animation

--- manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/dashboard/Dashboard.kt
+++ manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/dashboard/Dashboard.kt
@@ -1,13 +1,13 @@
 package io.github.manamiproject.manami.gui.dashboard
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import io.github.manamiproject.manami.gui.components.RotatingDotsProgress
 import io.github.manamiproject.manami.gui.components.simpletable.SimpleTable
 import io.github.manamiproject.manami.gui.extensions.toOnClick
 import io.github.manamiproject.manami.gui.theme.ManamiTheme
@@ -21,17 +21,21 @@ internal fun Dashboard(viewModel: DashboardViewModel = DashboardViewModel.instan
     val newVersion = viewModel.newVersion.collectAsState()
 
     ManamiTheme {
-        if (isLoading.value) {
-            Text("Loading")
-        } else {
-            if (newVersion.value.neitherNullNorBlank()) {
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                    Button(onClick = URI("https://github.com/manami-project/manami/releases/tag/${newVersion.value}").toOnClick()) {
-                        Text("\uD83D\uDE80 ${newVersion.value} available")
-                    }
+        if (newVersion.value.neitherNullNorBlank()) {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Button(onClick = URI("https://github.com/manami-project/manami/releases/tag/${newVersion.value}").toOnClick()) {
+                    Text("\uD83D\uDE80 ${newVersion.value} available")
                 }
             }
+        }
 
+        if (isLoading.value) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Row(modifier = Modifier.align(Alignment.Center)) {
+                    RotatingDotsProgress()
+                }
+            }
+        } else {
             SimpleTable(data.value) {
                 keyHeadline = "MetaDataProvider"
                 valueHeadline = "Number of anime"
