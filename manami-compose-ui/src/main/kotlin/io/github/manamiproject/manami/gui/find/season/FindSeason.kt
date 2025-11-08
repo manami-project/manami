ef448cd7c640f2361c2d83100e407905002a3a35
manami-project <13179375+manami-project@users.noreply.github.com>
2025-11-08 19:05:35 +0100

    4.0.0: Added possibility to search for seasons

--- manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/find/season/FindSeason.kt
+++ manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/find/season/FindSeason.kt
@@ -1,12 +1,163 @@
 package io.github.manamiproject.manami.gui.find.season
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.TextField
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.manamiproject.manami.gui.components.IconButton
+import io.github.manamiproject.manami.gui.components.RotatingDotsProgress
 import io.github.manamiproject.manami.gui.components.animetable.AnimeTable
 import io.github.manamiproject.manami.gui.theme.ManamiTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun FindSeason(viewModel: FindSeasonViewModel = FindSeasonViewModel.instance) {
+    val isSeasonSearchRunning = viewModel.isSeasonSearchRunning.collectAsState()
+
+    var metaDataProviderSelectExpanded by remember { mutableStateOf(false) }
+    val metaDataProviderOptions = viewModel.metaDataProviders()
+    val metaDataProviderSelectState = rememberTextFieldState(viewModel.metaDataProviders().first())
+
+    var seasonSelectExpanded by remember { mutableStateOf(false) }
+    val seasonOptions = viewModel.seasons()
+    val seasonSelectState = rememberTextFieldState(viewModel.currentSeason())
+
+    var yearSelectExpanded by remember { mutableStateOf(false) }
+    val yearOptions = viewModel.yearRange()
+    val yearSelectState = rememberTextFieldState(viewModel.currentYear().toString())
+
+    val padding = 5.dp
+
     ManamiTheme {
-        AnimeTable(viewModel = viewModel)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            ExposedDropdownMenuBox(
+                expanded = metaDataProviderSelectExpanded,
+                onExpandedChange = { metaDataProviderSelectExpanded = it },
+                modifier = Modifier.padding(0.dp, 0.dp,padding, 0.dp)
+            ) {
+                TextField(
+                    modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+                    state = metaDataProviderSelectState,
+                    readOnly = true,
+                    lineLimits = TextFieldLineLimits.SingleLine,
+                    label = { Text("MetaDataProvider") },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = metaDataProviderSelectExpanded) },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                )
+                ExposedDropdownMenu(expanded = metaDataProviderSelectExpanded, onDismissRequest = { metaDataProviderSelectExpanded = false }) {
+                    metaDataProviderOptions.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option, style = MaterialTheme.typography.bodyLarge) },
+                            onClick = {
+                                metaDataProviderSelectState.setTextAndPlaceCursorAtEnd(option)
+                                metaDataProviderSelectExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            ExposedDropdownMenuBox(
+                expanded = seasonSelectExpanded,
+                onExpandedChange = { seasonSelectExpanded = it },
+                modifier = Modifier.padding(0.dp, 0.dp,padding, 0.dp)
+            ) {
+                TextField(
+                    modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+                    state = seasonSelectState,
+                    readOnly = true,
+                    lineLimits = TextFieldLineLimits.SingleLine,
+                    label = { Text("Season") },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = seasonSelectExpanded) },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                )
+                ExposedDropdownMenu(expanded = seasonSelectExpanded, onDismissRequest = { seasonSelectExpanded = false }) {
+                    seasonOptions.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option.toString(), style = MaterialTheme.typography.bodyLarge) },
+                            onClick = {
+                                seasonSelectState.setTextAndPlaceCursorAtEnd(option.toString())
+                                seasonSelectExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            ExposedDropdownMenuBox(
+                expanded = yearSelectExpanded,
+                onExpandedChange = { yearSelectExpanded = it },
+                modifier = Modifier.padding(0.dp, 0.dp,padding, 0.dp)
+            ) {
+                TextField(
+                    modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable),
+                    state = yearSelectState,
+                    readOnly = true,
+                    lineLimits = TextFieldLineLimits.SingleLine,
+                    label = { Text("Year") },
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = yearSelectExpanded) },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                )
+                ExposedDropdownMenu(expanded = yearSelectExpanded, onDismissRequest = { yearSelectExpanded = false }) {
+                    yearOptions.forEach { option ->
+                        DropdownMenuItem(
+                            text = { Text(option.toString(), style = MaterialTheme.typography.bodyLarge) },
+                            onClick = {
+                                yearSelectState.setTextAndPlaceCursorAtEnd(option.toString())
+                                yearSelectExpanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            IconButton(
+                icon = Icons.Filled.Search,
+                size = 40.dp,
+                description = "Search",
+                onClick = { viewModel.search(metaDataProviderSelectState.text.toString(), seasonSelectState.text.toString(), yearSelectState.text.toString().toInt()) },
+            )
+        }
+
+        if (isSeasonSearchRunning.value) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Row(modifier = Modifier.align(Alignment.Center)) {
+                    RotatingDotsProgress()
+                }
+            }
+        } else {
+            AnimeTable(viewModel = viewModel)
+        }
     }
 }
--- manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/find/season/FindSeasonViewModel.kt
+++ manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/find/season/FindSeasonViewModel.kt
@@ -3,6 +3,10 @@ package io.github.manamiproject.manami.gui.find.season
 import io.github.manamiproject.manami.app.Manami
 import io.github.manamiproject.manami.app.events.SearchResultAnimeEntry
 import io.github.manamiproject.manami.gui.components.animetable.DefaultAnimeTableViewModel
+import io.github.manamiproject.modb.core.anime.AnimeSeason
+import io.github.manamiproject.modb.core.anime.YEAR_OF_THE_FIRST_ANIME
+import io.github.manamiproject.modb.core.anime.Year
+import io.github.manamiproject.modb.core.config.Hostname
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.SupervisorJob
@@ -10,10 +14,21 @@ import kotlinx.coroutines.flow.SharingStarted.Companion.Eagerly
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 
 internal class FindSeasonViewModel(private val app: Manami = Manami.instance): DefaultAnimeTableViewModel<SearchResultAnimeEntry>() {
 
     private val viewModelScope = CoroutineScope(Default + SupervisorJob())
+
+    val isSeasonSearchRunning: StateFlow<Boolean>
+        get() = app.findSeasonState
+            .map { it.isRunning }
+            .stateIn(
+                scope = viewModelScope,
+                started = Eagerly,
+                initialValue = false,
+            )
 
     override val source: StateFlow<List<SearchResultAnimeEntry>>
         get() = app.findSeasonState
@@ -25,6 +40,34 @@ internal class FindSeasonViewModel(private val app: Manami = Manami.instance): D
             )
 
     override fun delete(anime: SearchResultAnimeEntry) = throw UnsupportedOperationException()
+
+    fun yearRange(): List<Year> = (YEAR_OF_THE_FIRST_ANIME..LocalDate.now().year + 5).toList().reversed()
+
+    fun currentYear(): Year = LocalDate.now().year
+
+    fun metaDataProviders(): List<Hostname> = app.dashboardState.value.entries.toList().sortedByDescending { it.second }.map { it.first }
+
+    fun seasons() = listOf("Spring", "Summer", "Fall", "Winter")
+
+    fun currentSeason(): String {
+        return when(LocalDate.now().month.value) {
+            1, 2, 3 -> "Winter"
+            4, 5, 6 -> "Spring"
+            7, 8, 9 -> "Summer"
+            else -> "Fall"
+        }
+    }
+
+    fun search(metaDataProvider: Hostname, season: String, year: Year) {
+        val animeSeason = AnimeSeason(
+            season = AnimeSeason.Season.of(season),
+            year = year,
+        )
+
+        viewModelScope.launch {
+            app.findSeason(animeSeason, metaDataProvider)
+        }
+    }
 
     internal companion object {
         /**
