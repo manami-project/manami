774e44488a8b19fd06338c265db9926150aff4db
manami-project <13179375+manami-project@users.noreply.github.com>
2025-11-17 17:12:11 +0100

    4.0.0: Extended dashboard

--- manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/dashboard/DashboardViewModel.kt
+++ manami-compose-ui/src/main/kotlin/io/github/manamiproject/manami/gui/dashboard/DashboardViewModel.kt
@@ -1,6 +1,7 @@
 package io.github.manamiproject.manami.gui.dashboard
 
 import io.github.manamiproject.manami.app.Manami
+import io.github.manamiproject.manami.app.lists.Link
 import io.github.manamiproject.manami.gui.tabs.TabBarViewModel
 import io.github.manamiproject.manami.gui.tabs.Tabs.FIND_BY_TITLE
 import io.github.manamiproject.modb.core.anime.Title
@@ -12,9 +13,11 @@ import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.SharingStarted.Companion.Eagerly
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlin.math.round
 
 internal class DashboardViewModel(
     private val app: Manami = Manami.instance,
@@ -32,14 +35,37 @@ internal class DashboardViewModel(
                 initialValue = true,
             )
 
-    val metaDataProviderNumberOfAnime: StateFlow<Map<String, Int>>
-        get() = app.dashboardState
-            .map { event -> event.entries.toList().sortedByDescending { it.second }.toMap() }
+    val metaDataProviderNumberOfAnime: StateFlow<Map<String, String>>
+        get() = combine(app.dashboardState, app.animeListState, app.watchListState, app.ignoreListState) {
+            val animeListEntries = app.animeListState.value.entries
+            val watchListEntries = app.watchListState.value.entries
+            val ignoreListEntries = app.ignoreListState.value.entries
+
+            app.dashboardState.value.entries
+                .toList()
+                .sortedByDescending { it.second }
+                .toMap()
+                .map { (key, value) ->
+                    val numberOfAnimeListEntries = animeListEntries.filter { it.link is Link }.count { it.link.asLink().uri.host == key }
+                    val numberOfWatchListEntries = watchListEntries.count { it.link.uri.host == key }
+                    val numberOfIgnoreListEntries = ignoreListEntries.count { it.link.uri.host == key }
+                    val sumOfEntries = numberOfAnimeListEntries + numberOfWatchListEntries + numberOfIgnoreListEntries
+                    val percent = round(sumOfEntries.toDouble()/value.toDouble() * 100.00)
+                    val detailed = "$value | Anime List: $numberOfAnimeListEntries | Watch List: $numberOfWatchListEntries | Ignore List: $numberOfIgnoreListEntries | $percent %"
+
+                    key to if (sumOfEntries > 0) detailed else value.toString()
+                }
+                .toMap()
+
+        }.stateIn(viewModelScope, Eagerly, emptyMap<String, String>())
+
+            /*app.dashboardState
+            .map { event -> event.entries.toList()
             .stateIn(
                 scope = viewModelScope,
                 started = Eagerly,
                 initialValue = emptyMap(),
-            )
+            )*/
 
     val metaDataProviders: StateFlow<List<Hostname>>
         get() = app.dashboardState
