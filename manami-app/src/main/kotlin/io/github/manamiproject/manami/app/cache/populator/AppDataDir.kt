085d6095e4ba217273fd55fb1b4b9e97af3610aa
manami-project <13179375+manami-project@users.noreply.github.com>
2025-12-01 21:38:50 +0100

    4.0.0: Fixed race condition on startup

--- manami-app/src/main/kotlin/io/github/manamiproject/manami/app/cache/populator/AppDataDir.kt
+++ manami-app/src/main/kotlin/io/github/manamiproject/manami/app/cache/populator/AppDataDir.kt
@@ -3,14 +3,14 @@ package io.github.manamiproject.manami.app.cache.populator
 import io.github.manamiproject.modb.core.extensions.directoryExists
 import java.nio.file.Path
 import kotlin.io.path.Path
-import kotlin.io.path.createDirectory
+import kotlin.io.path.createDirectories
 
 fun appDataDir(path: Path): Path {
     val home = System.getProperty("user.home") ?: throw IllegalStateException("user.home not set")
     val appDir = Path(home).resolve(".manami")
 
     if (!appDir.directoryExists()) {
-        appDir.createDirectory()
+        appDir.createDirectories()
     }
 
     return appDir.resolve(path)
