c013688cf7dccfa835c3d1cbbdf733cf7d2a42cb
manami-project <13179375+manami-project@users.noreply.github.com>
2026-06-14 19:16:34 +0200

    Updated kotlinVersion in build.gradle.kts to 2.4.0

--- manami-compose-ui/build.gradle.kts
+++ manami-compose-ui/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 val githubUsername = "manami-project"
-val kotlinVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3 // most recent stable kotlin version for language and std lib
+val kotlinVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_4 // most recent stable kotlin version for language and std lib
 
 repositories {
     google()
